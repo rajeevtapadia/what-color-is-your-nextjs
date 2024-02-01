83b9bff5f00da3b3a3c6404db154c50b797efdd3
Rajeev Tapadia <rtapadia730@gmail.com>
2024-02-01 23:36:19 +0530

    idk

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+// server vs client components
+
+// all components are server by default
+// but server comp cant aceess browser apis or listen to browser events
+// usually we default to server components in next but use client only when necessary
+
 import Link from "next/link";
+import AddToCart from "./components/AddToCart";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>Homepage</h1>
+      {/* dont put a anchor tag it re request the assests */}
       <Link href={'/login'}>loginpage</Link>
+      <br/><br/>
+
+      <div>
+        <AddToCart/>
+      </div>
     </main>
   );
 }
